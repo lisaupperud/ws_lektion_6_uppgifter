13c6aff411e5fd3622ce7dbadc7a3697d5edd6f7
liluppis <lisa.upp@hotmail.com>
2025-09-01 11:39:13 +0200

    add: CustomUserController with GET, PUT & POST Mappings, refactor: findByUsername() to use Optional<> instead of List<> in CustomUserRepository for better error handling

---
+++ src/main/java/com/lisa/lektion_6_uppgifter/controller/CustomUserController.java
@@ -0,0 +1,63 @@
+package com.lisa.lektion_6_uppgifter.controller;
+
+import com.lisa.lektion_6_uppgifter.model.CustomUser;
+import com.lisa.lektion_6_uppgifter.repository.CustomUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/v1/user")
+public class CustomUserController {
+
+    @Autowired
+    CustomUserRepository customUserRepository;
+
+    @GetMapping("/")
+    public ResponseEntity<List<CustomUser>> findAll() {
+
+        List<CustomUser> userList = customUserRepository.findAll();
+
+        if (userList.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(userList);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<CustomUser> addUser(@RequestBody CustomUser customUser) {
+
+        // if the user already exists
+        if (customUserRepository.findByUsername(customUser.getUsername()).isPresent()) {
+            return ResponseEntity.unprocessableEntity().build();
+        }
+
+        CustomUser savedUser = customUserRepository.save(customUser);
+        return ResponseEntity.ok(savedUser);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CustomUser> updateUser(@PathVariable Long id, @RequestBody CustomUser customUser) {
+
+        Optional<CustomUser> foundUser = customUserRepository.findById(id);
+
+        if(foundUser.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        CustomUser userToBeUpdated = foundUser.get();
+
+        userToBeUpdated.setUsername(customUser.getUsername());
+        userToBeUpdated.setPassword(customUser.getPassword());
+        userToBeUpdated.setEnabled(customUser.isEnabled());
+
+        CustomUser updatedUser = customUserRepository.save(userToBeUpdated);
+
+        return ResponseEntity.ok(updatedUser);
+
+    }
+}
--- src/main/java/com/lisa/lektion_6_uppgifter/repository/CustomUserRepository.java
+++ src/main/java/com/lisa/lektion_6_uppgifter/repository/CustomUserRepository.java
@@ -3,9 +3,9 @@ package com.lisa.lektion_6_uppgifter.repository;
 import com.lisa.lektion_6_uppgifter.model.CustomUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface CustomUserRepository extends JpaRepository<CustomUser,Long> {
 
-    List<CustomUser> findByUsername(String username);
+    Optional<CustomUser> findByUsername(String username);
 }
