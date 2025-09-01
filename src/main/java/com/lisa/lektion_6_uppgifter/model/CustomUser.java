4c0057da2d6d310a14f38f6eef8ee36a82dcbe00
liluppis <lisa.upp@hotmail.com>
2025-09-01 11:00:47 +0200

    add: example application.properties file

---
+++ src/main/java/com/lisa/lektion_6_uppgifter/model/CustomUser.java
@@ -0,0 +1,60 @@
+package com.lisa.lektion_6_uppgifter.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Users")
+public class CustomUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private boolean isEnabled;
+
+    public CustomUser() {}
+
+    public CustomUser(String username, String password, boolean isEnabled) {
+        this.username = username;
+        this.password = password;
+        this.isEnabled = isEnabled;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        isEnabled = enabled;
+    }
+
+}
